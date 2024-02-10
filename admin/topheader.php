780e433d501f555270aab04031894f12a205d96a
Leva_Dokhoyan <leva.dokhoyan@contracted.pmru.com>
2024-02-10 23:48:23 +0400

    Permissions system implemented

--- admin/topheader.php
+++ admin/topheader.php
@@ -4,8 +4,19 @@
       <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top ">
         <div class="container-fluid">
           <div class="navbar-wrapper">
-            <a class="navbar-brand" href="javascript:void(0)">Admin</a>
-            
+             <?php
+             include "../db.php";
+             if (isset($_SESSION["uid"])) {
+                 $sql = "SELECT first_name FROM user_info WHERE user_id='$_SESSION[uid]'";
+                 $query = mysqli_query($con, $sql);
+                 $row = mysqli_fetch_array($query);
+
+                 echo '
+                    <div class="dropdownn">
+                        <a class="navbar-brand" href="javascript:void(0)">Привет ' . $row["first_name"] . ' !</a>
+                    </div>';
+            }
+             ?>
           </div>
           <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
             <span class="sr-only">Toggle navigation</span>
@@ -13,21 +24,6 @@
             <span class="navbar-toggler-icon icon-bar"></span>
             <span class="navbar-toggler-icon icon-bar"></span>
           </button>
-          <div class="collapse navbar-collapse justify-content-end">
-            <ul class="navbar-nav">
-              <li class="nav-item dropdown">
-                <a class="nav-link" href="javscript:void(0)" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  <i class="material-icons">person</i>
-                  
-                  <p class="d-lg-none d-md-block">
-                    Some Actions
-                  </p>
-                </a>
-                
-              </li>
-              <!-- your navbar here -->
-            </ul>
-          </div>
         </div>
       </nav>
       
